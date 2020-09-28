6616a77278497b54abb17e0cd9cc190f385880df
Edgardo Rios <edgardo.rios@protonmail.com>
2020-09-28 17:43:48 -0500

    Updates

---
+++ precache-manifest.cf70d43ccb6ca9678cc05e582844e9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76a4bb023d99a4afd2bf78b4ced6a50",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "545b1bc3c7b7a9c9b64c",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545b1bc3c7b7a9c9b64c",
+    "url": "/tic-tac-toe/static/js/main.423d0c3f.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
